643cd74ddb30d0bc0d216e09317ab298ccde5994
DanielOgnyanov <escape69@abv.bg>
2025-07-25 17:23:35 +0300

    create initial user inside db

--- src/main/java/org/example/custodiacrm/service/Impl/UserServiceImpl.java
+++ src/main/java/org/example/custodiacrm/service/Impl/UserServiceImpl.java
@@ -1,6 +1,43 @@
 package org.example.custodiacrm.service.Impl;
 
+import org.example.custodiacrm.models.entities.User;
+import org.example.custodiacrm.models.enums.UserRole;
+import org.example.custodiacrm.repositories.UserRepository;
 import org.example.custodiacrm.service.UserService;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+
+
+    @Override
+    public void createInitialUser() {
+        if (!userRepository.existsByEmail("admin@example.com")) {
+
+            User user = User.builder()
+                    .username("admin")
+                    .firstName("System")
+                    .lastName("Admin")
+                    .email("admin@example.com")
+                    .password(passwordEncoder.encode("admin123"))
+                    .role(UserRole.ADMIN)
+                    .phoneNumber("+1234567890")
+                    .build();
+
+
+            userRepository.save(user);
+        }
+    }
 }
--- src/main/java/org/example/custodiacrm/service/UserService.java
+++ src/main/java/org/example/custodiacrm/service/UserService.java
@@ -1,4 +1,6 @@
 package org.example.custodiacrm.service;
 
 public interface UserService {
+
+    void createInitialUser();
 }
