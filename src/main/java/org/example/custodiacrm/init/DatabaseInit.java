c2a164fb9a825aa00d678aa9f408165c246c9995
DanielOgnyanov <escape69@abv.bg>
2025-07-25 17:30:55 +0300

    create initial user inside db

--- src/main/java/org/example/custodiacrm/init/DatabaseInit.java
+++ src/main/java/org/example/custodiacrm/init/DatabaseInit.java
@@ -1,8 +1,21 @@
 package org.example.custodiacrm.init;
 
 
+import jakarta.annotation.PostConstruct;
+import org.example.custodiacrm.service.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseInit {
+
+    private final UserService userService;
+
+    public DatabaseInit(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void init() {
+        userService.createInitialUser();
+    }
 }
