91cb4bab962a2f00bd142614a9203582b9b005b5
DanielOgnyanov <escape69@abv.bg>
2025-07-26 15:54:22 +0300

    user register dto

---
+++ src/main/java/org/example/custodiacrm/models/dto/UserRegisterDto.java
@@ -0,0 +1,15 @@
+package org.example.custodiacrm.models.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserRegisterDto {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+}
--- src/main/java/org/example/custodiacrm/service/Impl/UserServiceImpl.java
+++ src/main/java/org/example/custodiacrm/service/Impl/UserServiceImpl.java
@@ -40,4 +40,7 @@ public class UserServiceImpl implements UserService {
             userRepository.save(user);
         }
     }
+
+
+
 }
