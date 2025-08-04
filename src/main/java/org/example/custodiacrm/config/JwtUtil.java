9a6b5e07f7a5295e66c32ebe3f693bdbfaa7e79f
DanielOgnyanov <escape69@abv.bg>
2025-08-05 02:27:17 +0300

    change the key generation for jwt

--- src/main/java/org/example/custodiacrm/config/JwtUtil.java
+++ src/main/java/org/example/custodiacrm/config/JwtUtil.java
@@ -3,15 +3,17 @@ package org.example.custodiacrm.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String jwtSecret = "SecretKey";
+    private static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     private final long jwtExpirationMs = 86400000;
 
     public String generateToken(String email) {
@@ -19,7 +21,7 @@ public class JwtUtil {
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(key)
                 .compact();
     }
 }
